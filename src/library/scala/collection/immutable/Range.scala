084bbf997b9470ffb43e400290b06a2d056384a4
Guillaume Martres <smarter@ubuntu.com>
2017-08-09 18:08:33 +0200

    Range#foreach cannot be an inline method
    Because explicit returns are not allowed in inline methods

--- src/library/scala/collection/immutable/Range.scala
+++ src/library/scala/collection/immutable/Range.scala
@@ -147,7 +147,7 @@ extends scala.collection.AbstractSeq[Int]
     else start + (step * idx)
   }
 
-  @inline final override def foreach[@specialized(Unit) U](f: Int => U) {
+  final override def foreach[@specialized(Unit) U](f: Int => U) {
     // Implementation chosen on the basis of favorable microbenchmarks
     // Note--initialization catches step == 0 so we don't need to here
     if (!isEmpty) {
