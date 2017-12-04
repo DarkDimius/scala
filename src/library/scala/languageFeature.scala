c8b20748e3c2029178d236953fd5ba06b0b4845b
Martin Odersky <odersky@gmail.com>
2017-12-04 15:50:26 +0100

    Change references to `meta` so that a `scala.mete` package can be added.

--- src/library/scala/languageFeature.scala
+++ src/library/scala/languageFeature.scala
@@ -5,41 +5,37 @@
 ** /____/\___/_/ |_/____/_/ | |                                         **
 **                          |/                                          **
 \*                                                                      */
-
-
-
 package scala
-
-import scala.annotation.meta
+import scala.annotation.meta.{languageFeature => feature}
 
 object languageFeature {
 
-  @meta.languageFeature("extension of type scala.Dynamic", enableRequired = true)
+  @feature("extension of type scala.Dynamic", enableRequired = true)
   sealed trait dynamics
   object dynamics extends dynamics
 
-  @meta.languageFeature("postfix operator #", enableRequired = false)
+  @feature("postfix operator #", enableRequired = false)
   sealed trait postfixOps
   object postfixOps extends postfixOps
 
-  @meta.languageFeature("reflective access of structural type member #", enableRequired = false)
+  @feature("reflective access of structural type member #", enableRequired = false)
   sealed trait reflectiveCalls
   object reflectiveCalls extends reflectiveCalls
 
-  @meta.languageFeature("implicit conversion #", enableRequired = false)
+  @feature("implicit conversion #", enableRequired = false)
   sealed trait implicitConversions
   object implicitConversions extends implicitConversions
 
-  @meta.languageFeature("higher-kinded type", enableRequired = false)
+  @feature("higher-kinded type", enableRequired = false)
   sealed trait higherKinds
   object higherKinds extends higherKinds
 
-  @meta.languageFeature("#, which cannot be expressed by wildcards, ", enableRequired = false)
+  @feature("#, which cannot be expressed by wildcards, ", enableRequired = false)
   sealed trait existentials
   object existentials extends existentials
 
   object experimental {
-    @meta.languageFeature("macro definition", enableRequired = true)
+    @feature("macro definition", enableRequired = true)
     sealed trait macros
     object macros extends macros
   }
